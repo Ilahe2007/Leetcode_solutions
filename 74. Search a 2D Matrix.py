36ebce94f53cf2e53f1879f4138af2a86468ebb6
İlaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2023-06-03 15:41:47 +0400

    Create 74. Search a 2D Matrix.py

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in matrix:
+            a=list(filter(lambda x:x==target,i))
+            if len(a)!=0:
+                return True
+                break
+        return False
